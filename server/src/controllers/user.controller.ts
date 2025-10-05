059ae48d8d16ddecfb168910e2cd4515d3078126
theodore-fiseha <teddytez000@gmail.com>
2025-10-05 18:05:57 +0300

    user controller setup for authentication

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+// @desc Login User
+// @route POST /api/auth/login
+// @access Public
+export const userLogin = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+// @desc Register new User
+// @route POST /api/auth/signup
+// @access Public
+export const userRegister = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+// @desc User Logout
+// @route POST /api/auth/logout
+// @access Public
+export const userLogout = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
